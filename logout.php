47d9eb14b386d71cea76ce4fa19bb71db3e0e05b
Daniel Banega <danielbcenturion@gmail.com>
2018-05-29 20:10:09 -0300

    deberia de funcionar.

--- logout.php
+++ logout.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 session_start();
@@ -7,13 +6,3 @@ header('Location: index.php');
 exit;
 
  ?>
-=======
-<?php
-
-session_start();
-session_destroy();
-header('Location: index.php');
-exit;
-
- ?>
->>>>>>> a4ce69f72c65ccb2c8ecb43fe3b01c421ca933b3
